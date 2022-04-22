155ef13b8510709471deb0207f42e80af940a73c
joshuaclausen <joshuaclausen@users.noreply.github.com>
2022-04-21 17:07:17 -0700

    comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # blinkhome
+
+
+Purpose: periodically take pictures from all cameras so we can make a time lapse video
+
+# Prereqs
+- python3 (or python2)
+- blinkpy installed via pip
+```
+pip3 install blinkpy
+```
+- Runs on Windows, Linux, and MacOS.
+
+
+# How to run
+```
+python3 main.py
+```
+
+- It's a simple script that you need to run once manually.
+- The blinkpy package prompts for username/password/2fa, then this script saves the resulting creds in a file name saved_secrets.json in the same directory.
+- The next time the script runs, it will just load up those creds and run without needing user input.
+
+
+# Extending
+- Right now it is hard-coded to take a picture every 10 seconds, but that could be made variable very easily.
+
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-
+# prereq is the blinkpy package installed via pip3
+# pip3 install blinkpy
+# run this with "python3 main.py"
 
 
 from time import sleep
@@ -8,28 +10,49 @@ from blinkpy.helpers.util import json_load
 from datetime import datetime
 from time import sleep
 
-
+# init
 blink = Blink()
+
+# load the auth file - if the file doesn't exist, it just asks for creds
+# later we save the file use future use
 auth = Auth(json_load("./saved_secrets.json"))
+
+# authorize with blink
 blink.auth = auth
+
+# start things up
 blink.start()
 
 
-
-
+# save the auth file, now that we've logged in
 blink.save("./saved_secrets.json")
 
+
+# now do an infinite loop
 while(True):
+
+    # loop through all cameras in the account
     for name, camera in blink.cameras.items():
         print(name)                   # Name of the camera
         print(camera.attributes)      # Print available attributes of camera
-        camera = blink.cameras[''+ name + '']
+
+        # load up the current camera - note we are concatenating the camera name - it didn't work without doing it this way
+        camera = blink.cameras['' + name + '']
+
+        # tell the camera to take a new picture
         camera.snap_picture()
+
+        # refresh with the blink service, so later we can get the image
         blink.refresh()
+
+        # get the current time, so we can add it to the file name
         now = datetime.now()
         cur_time = now.strftime("%Y_%m_%d_%H_%M_%S")
+
+        # get the image and save it as a new file with the timestamp
         camera.image_to_file('./' + name + '_' + cur_time +'.jpg')
 
-    sleep(30)
+    # wait awhile
+    sleep(10)
 
 
